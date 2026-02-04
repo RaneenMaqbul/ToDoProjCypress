661672f82b6ab0494cb7c74a2c3526c2cbd5eb8a
Raneen Maqbul <159143815+RaneenMaqbul@users.noreply.github.com>
2026-02-04 15:14:06 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,33 +3,27 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "kpj6do",
 
-  // ✅ Mochawesome Reporter (مرة واحدة فقط)
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
-    reportDir: "cypress/reports",   // ✅ بدون .jsons
-    reportFilename: "[name]-report",
+    reportDir: "cypress/reports",
     overwrite: false,
-    html: false,                    // نخلي HTML يتولد بعد الدمج
-    json: true,                     // نخلي JSON ينولد لكل spec
+    html: false,
+    json: true,
     charts: true,
     reportPageTitle: "Automation Test Report",
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
-    quiet: true
   },
 
   e2e: {
-    downloadsFolder: "cypress/downloads",
-    chromeWebSecurity: false,
-
     env: {
       email: "raneen-test@gmail.com",
       password: "Raneen@123456",
     },
-
+    downloadsFolder: "cypress/downloads",
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-      // ✅ plugin لازم يكون جوّا setupNodeEvents
       require("cypress-mochawesome-reporter/plugin")(on);
       return config;
     },
